39a7ff1380146b4e2651811ae732facaf9f9c4d9
rovo98 <2402323937@qq.com>
2018-03-19 12:45:31 +0800

    update files in base/

---
+++ base/multi_lines_print.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# author: rovo98
+# date: 2018.3.18
+
+# print a string.
+print('I\'m OK.\n')
+
+# print the  multiply lines
+print('''line1
+line2,
+line3
+...''')
+
+# print the raw multiply lines
+print(r'''hello, \n
+world''')
