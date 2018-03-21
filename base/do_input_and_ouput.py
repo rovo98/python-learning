88897241c605a522aa93c4df318a0ee04466e769
rovo98 <2402323937@qq.com>
2018-03-21 13:35:36 +0800

    update folders

--- base/do_input_and_ouput.py
+++
@@ -1,9 +0,0 @@
-# -*- coding:utf-8 -*-
-# author: rovo98
-# date: 2018.3.18
-
-# do a input and output test.
-name = input('please enter your name:')
-print('hello',name,'!')
-
-print('1024 * 768 = ', 1024 * 768)
